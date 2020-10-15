3b08236234198821373cca12c5fddaff73b80b32
FENIX194 <leonardomatamala123@gmail.com>
2020-10-14 22:02:33 -0300

    COMMIT: VERSION 1

---
+++ app/src/main/java/com/fenix194/calculadora/MainActivity.java
@@ -0,0 +1,37 @@
+package com.fenix194.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button A;
+    Button B;
+    Button C;
+    Button D;
+    Button F;
+    Button G;
+    Button H;
+    Button I;
+    Button Z;
+    Button SUMA;
+    Button RESTA;
+    Button MULTI;
+    Button DIVICION;
+    Button RESULTADO;
+    TextView MOSTRAR;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
